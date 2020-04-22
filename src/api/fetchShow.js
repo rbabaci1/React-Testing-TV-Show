9e5b572552710418c314acfad13825fe6f3c097b
rabah babaci <4rabah@gmail.com>
2020-04-22 11:12:04 -0700

    move fetchShow to its own file

---
+++ src/api/fetchShow.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const fetchShow = () => {
+  axios
+    .get(
+      'https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes'
+    )
+    .then((res) => res)
+    .catch((err) => err);
+};
+
+export default fetchShow;
