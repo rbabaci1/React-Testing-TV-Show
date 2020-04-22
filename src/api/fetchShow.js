badc4ba40f08846bbb8b4005b89f757758bebddc
rabah babaci <4rabah@gmail.com>
2020-04-22 11:22:58 -0700

    import fetchShow into App and set the state with the data received

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const fetchShow = () => {
+const fetchShow = () =>
   axios
     .get(
       'https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes'
     )
     .then((res) => res)
-    .catch((err) => err);
-};
+    .catch((err) => console.error(err));
 
 export default fetchShow;
