88000e6b5c746a0ed982e9fe54d3c482258d1296
rabah babaci <4rabah@gmail.com>
2020-04-23 12:08:48 -0700

    define the formatSeason test

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchShow = async () =>
   await axios.get(
-    'https://api.tvqmaze.com/singlesearch/shows?q=stranger-things&embed=episodes'
+    'https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes'
   );
 
 export default fetchShow;
---
+++ src/utils/formatSeason.test.js
@@ -0,0 +1,1 @@
+it('formats the seasons correctly', () => {});
