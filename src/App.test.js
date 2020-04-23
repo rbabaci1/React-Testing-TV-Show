5cd6e8a7860eb472e1224a365302172991a4a3a7
rabah babaci <4rabah@gmail.com>
2020-04-23 10:45:47 -0700

    place mockData and react-dropdown mock in external folder

--- src/App.test.js
+++ src/App.test.js
@@ -8,62 +8,16 @@ import {
 } from '@testing-library/react';
 
 import { fetchShow as mockFetchShow } from './api/fetchShow';
+import mockDropdown from 'react-dropdown';
 import App from './App';
-
-const mockEpisodesData = [
-  {
-    id: 101,
-    name: 'chapter 11',
-    runtime: 90,
-    season: 99,
-    number: 11,
-    image: { medium: '/first' },
-    summary: '<p>just watch it</p>',
-  },
-  {
-    id: 102,
-    name: 'chapter 12',
-    runtime: 100,
-    season: 99,
-    number: 12,
-    image: { medium: '/second' },
-    summary: '<p>do not watch it</p>',
-  },
-];
-
-const mockShowData = {
-  data: {
-    name: 'rabah',
-    image: { original: '/original' },
-    summary: '<p>A love letter<p>',
-    _embedded: {
-      episodes: mockEpisodesData,
-    },
-  },
-};
+import { mockData } from './mockData.js';
 
 afterEach(cleanup);
-
-jest.mock('react-dropdown', () => ({ options, value, onChange }) => {
-  return (
-    <select
-      value={value}
-      onChange={(e) => onChange(e.target)}
-      data-testid='select'
-    >
-      {options.map((option, i) => (
-        <option key={i} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
-  );
-});
 
 jest.mock('./api/fetchShow');
 
 it('fetches and renders the data', async () => {
-  mockFetchShow.mockResolvedValueOnce(mockShowData);
+  mockFetchShow.mockResolvedValueOnce(mockData);
 
   const { getByTestId, getAllByTestId } = render(<App />);
 
